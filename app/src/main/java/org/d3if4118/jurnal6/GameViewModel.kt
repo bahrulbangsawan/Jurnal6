d159db1f77c8913ca695b646f3277f654961440b
bahrulbangsawan <bahrulbangsawan@gmail.com>
2020-03-09 11:39:42 +0700

    live data

---
+++ app/src/main/java/org/d3if4118/jurnal6/GameViewModel.kt
@@ -0,0 +1,25 @@
+package org.d3if4118.jurnal6
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel(){
+
+    private var _scoreTeamA = MutableLiveData<Int>()
+        val scoreTeamA : LiveData<Int>
+        get() = _scoreTeamA
+
+    private var _scoreTeamB = MutableLiveData<Int>()
+        val scoreTeamB : LiveData<Int>
+        get() = _scoreTeamB
+
+    init {
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+
+}
--- app/src/main/java/org/d3if4118/jurnal6/MainActivity.kt
+++ app/src/main/java/org/d3if4118/jurnal6/MainActivity.kt
@@ -2,11 +2,64 @@ package org.d3if4118.jurnal6
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: GameViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+    internal var scoreTeamA = 0
+
+    internal var scoreTeamB = 0
+    fun addOneForTeamA(v: View) {
+        scoreTeamA = scoreTeamA + 1
+        displayForTeamA(scoreTeamA)
+    }
+
+    fun addTwoForTeamA(v: View) {
+        scoreTeamA = scoreTeamA + 2
+        displayForTeamA(scoreTeamA)
+    }
+
+    fun addThreeForTeamA(v: View) {
+        scoreTeamA = scoreTeamA + 3
+        displayForTeamA(scoreTeamA)
+    }
+
+    fun addOneForTeamB(v: View) {
+        scoreTeamB = scoreTeamB + 1
+        displayForTeamB(scoreTeamB)
+    }
+
+    fun addTwoForTeamB(v: View) {
+        scoreTeamB = scoreTeamB + 2
+        displayForTeamB(scoreTeamB)
+    }
+
+    fun addThreeForTeamB(v: View) {
+        scoreTeamB = scoreTeamB + 3
+        displayForTeamB(scoreTeamB)
+    }
+
+
+    fun resetScore(v: View) {
+        scoreTeamA = 0
+        scoreTeamB = 0
+        displayForTeamA(scoreTeamA)
+        displayForTeamB(scoreTeamB)
+    }
+    fun displayForTeamA(score: Int) {
+        val scoreView = findViewById(R.id.team_a_score) as TextView
+        scoreView.text = score.toString()
+    }
+    fun displayForTeamB(score: Int) {
+        val scoreView = findViewById(R.id.team_b_score) as TextView
+        scoreView.text = score.toString()
+    }
+
 }
